1c3f593cbb0663f42cbf1d661a7735100f9ffc88
wprince0288 <wprince0288@gmail.com>
2024-10-27 16:44:01 -0500

    complete

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,24 +1,43 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React, { useReducer } from 'react' // 👈 you'll need the reducer hook
 
 // 👇 these are the types of actions that can change state
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
 // 👇 create your initial state object here
-
+const initialState = {
+  authorName: '',
+  quoteText: '',
+}
 // 👇 create your reducer function here
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value } = action.payload
+      return { ...state, [name]: value }
+    }
+    case RESET_FORM: {
+      return { authorName: '', quoteText: '' }
+    }
+    default: {
+      return state
+    }
+  }
+}
 
-export default function TodoForm({ createQuote = () => { } }) {
+export default function TodoForm({ createQuote }) {
   // 👇 use the reducer hook to spin up state and dispatch
-
-  const onChange = () => {
-    // 👇 implement
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const onChange = ({ target: { name, value } }) => {
+    dispatch({ type: CHANGE_INPUT, payload: { name, value } })
   }
   const resetForm = () => {
-    // 👇 implement
+    dispatch({ type: RESET_FORM })
   }
-  const onNewQuote = () => {
-    // 👇 implement
+  const onNewQuote = (evt) => {
+    evt.preventDefault()
+    const { authorName, quoteText } = state
+    createQuote({ authorName, quoteText })
     resetForm()
   }
 
@@ -29,6 +48,7 @@ export default function TodoForm({ createQuote = () => { } }) {
       <label><span>Author:</span>
         <input
           type='text'
+          value={state.authorName}
           name='authorName'
           placeholder='type author name'
           onChange={onChange}
